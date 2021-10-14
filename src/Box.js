985f683217f15e169da87be3baebfda7298a0fbb
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2021-10-14 09:06:44 +0200

    prepare editing boxes

--- src/Box.js
+++ src/Box.js
@@ -1,4 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField
+} from "@mui/material";
 
 const Box = ({
   item,
@@ -8,22 +16,49 @@ const Box = ({
   startNewItem,
   level
 }) => {
+  const [editDialogOpen, setEditDialogOpen] = useState(false);
+  const [fill, setFill] = useState("rgba(0,0,0,0.3");
+  const [label, setLabel] = useState("");
+
   return (
-    <rect
-      x={item.position * factorX}
-      y={0}
-      width={item.duration * factorX}
-      height={item.duration * factorY}
-      style={{
-        fill: item.selected ? `rgba(250, 0, 0, 0.3)` : `rgba(0,0,0, 0.3)`,
-        strokeWidth: "1",
-        stroke: "rgb(0,0,0)"
-      }}
-      onClick={(e) => {
-        if (e.shiftKey) connectToLastItem(item);
-        else startNewItem(item);
-      }}
-    />
+    <>
+      <rect
+        x={item.position * factorX}
+        y={0}
+        width={item.duration * factorX}
+        height={item.duration * factorY}
+        style={{
+          fill,
+          strokeWidth: item.selected ? "2" : "1",
+          stroke: "black"
+        }}
+        onClick={(e) => {
+          if (e.altKey) {
+            setEditDialogOpen(true);
+            return;
+          }
+
+          if (e.shiftKey) connectToLastItem(item);
+          else startNewItem(item);
+        }}
+      />
+
+      <text>{label}</text>
+
+      <Dialog open={editDialogOpen} onClose={() => setEditDialogOpen(false)}>
+        <DialogTitle>Edit Box</DialogTitle>
+
+        <DialogContent>
+          <TextField label="Label" size="small" />
+          <TextField label="Color" size="small" />
+        </DialogContent>
+
+        <DialogActions>
+          <Button>Apply</Button>
+          <Button>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
